40eb9bc9fffc02f958e68b88d5bd890936b66a44
mr.eshtaiwi <mr.eshtaiwi@gmail.com>
2020-06-23 14:57:37 +0300

    adding js

---
+++ js/myscript.js
@@ -0,0 +1,26 @@
+'use strict';
+// function wait(ms) {
+//   var start = new Date().getTime();
+//   var end = start;
+//   while (end < start + ms) {
+//     end = new Date().getTime();
+//   }
+//   alert('what is your name');
+// }
+// wait(7000);
+
+// alert('hello');
+// console.log('hello from js file');
+
+//data type
+var name = 'hasan';//string
+var age = 28;//number 
+var loveCoding = true;//boolean
+
+//
+var job;//0 null random undefined 
+console.log('job = ',job);
+// alert('job = ',job);
+
+console.log('name = ',name);
+// var nameofthevariable = 10;
